73f5700c88ee6a724162df6c2f7e8c7e306d5c84
Luke BC <lbidigarecurtis@alumni.berklee.edu>
2017-11-08 08:28:11 -0500

    working on user sign up. go devise working, but still need to seperate the user sign up pages so they have their own content and redirect to the right home page variations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :musical_entities
-  devise_for :venues
-  devise_for :listeners
-  
+  devise_for :musical_entities, path: 'musical_entities'
+  devise_for :venues, path: 'venues'
+  devise_for :listeners, path: 'listeners'
+
   root 'static_pages#home'
 
   get 'static_pages/home'
